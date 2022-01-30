e3e10932a4c77a0724cf2dc2c384400ccfccffcc
George Middleton <georgemiddleton3@gmail.com>
2022-01-30 12:33:40 -0700

    Add first & last name to user and forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
