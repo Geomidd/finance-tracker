26671e9f3197b0a7afc7506f283d154e5db9a827
George Middleton <georgemiddleton3@gmail.com>
2022-01-29 20:13:15 -0700

    Add UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,15 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex[:publishable_key], endpoint: "https://sandbox.iexapis.com/v1")
     begin
       quote = client.quote(ticker_symbol)
       new(ticker: quote.symbol, name: quote.company_name, last_price: quote.latest_price)
-    rescue => exception
+    rescue
       return nil
     end
   end
